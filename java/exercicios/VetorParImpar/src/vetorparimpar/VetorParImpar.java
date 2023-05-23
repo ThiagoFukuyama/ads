4ae58807a52f01d1dadca0d814a5445c60cc794c
Thiago Fukuyama Marcilli <thiagofkm@gmail.com>
2023-05-23 16:21:04 -0300

    Finalizado exercício VetorParImpar

--- java/exercicios/VetorParImpar/src/vetorparimpar/VetorParImpar.java
+++ java/exercicios/VetorParImpar/src/vetorparimpar/VetorParImpar.java
@@ -19,16 +19,50 @@ public class VetorParImpar {
 
         Scanner scan = new Scanner(System.in);
 
-        System.out.println("Digite 15 valores inteiros do vetor: ");
+        System.out.println("Digite os 15 valores inteiros do vetor: ");
         for (int i = 0; i < 15; i++) {
 
-            System.out.print(i + "Âº - ");
+            System.out.print((i + 1) + "º - ");
             number = scan.nextInt();
 
             if (number % 2 == 0) {
-                // evenNumbers[]
+                
+                evenNumbers[evenCounter] = number;
+                evenCounter++;
+                if (evenCounter > 4) {
+                    System.out.println("Números pares até agora: ");
+                    for (int j = 0; j < evenNumbers.length; j++) {
+                        System.out.print(evenNumbers[j] + " ");
+                    }
+                    System.out.println(System.lineSeparator());
+                    evenCounter = 0;
+                }
+                
+            } else {
+                
+                oddNumbers[oddCounter] = number;
+                oddCounter++;         
+                if (oddCounter > 4) {
+                    System.out.println("Números ímpares até agora: ");    
+                    for (int j = 0; j < oddNumbers.length; j++) {
+                        System.out.print(oddNumbers[j] + " ");
+                    }
+                    System.out.println(System.lineSeparator());                    
+                    oddCounter = 0;
+                }
+                
             }
 
+        }
+        
+        System.out.println("Conteúdo final dos pares: ");
+        for (int i = 0; i < evenNumbers.length; i++) {
+            System.out.print(evenNumbers[i] + " ");
+        }
+        
+        System.out.println("\nConteúdo final dos ímpares: ");
+        for (int i = 0; i < oddNumbers.length; i++) {
+            System.out.print(oddNumbers[i] + " ");
         }
 
         scan.close();
